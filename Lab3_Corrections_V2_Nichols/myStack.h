d8010308bc5047bc4dda19522742c000b4d94597
EuclideanDreamer <78236576+EuclideanDreamer@users.noreply.github.com>
2021-11-11 21:55:58 -0800

    Add files via upload

---
+++ Lab3_Corrections_V2_Nichols/myStack.h
@@ -0,0 +1,190 @@
+/*
+'* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+'* Program name    :  mystack.h                                               * 
+'*                                                                     * 
+'* Written by      : Bailey Nichols                                     * 
+'*                                                                     * 
+'* Purpose         :   The purpose of this program is to                             * 
+'*                                                                     * 
+'*                                                                     * 
+'* Structure       : BEGIN                                             * 
+'*                        while not at the end of the data             *
+'*                            1 init the stack     
+                              2 process the expression
+                              3 output result
+                              4 get next expression                    *
+'*                   STOP                                              * 
+'*                        End of Program                               * 
+'*                                                                     * 
+'*---------------------------------------------------------------------* 
+'* Change Log:                                                         * 
+'*                         Revision                                    * 
+'*       Date    Changed  Rel Ver Mod Purpose                          * 
+'* 11/08/12      Nichols 000.000.000 Initial release of program       * 
+'*                                                                     * 
+'* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*/
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+template <class T>
+class stackNode{    
+    public:
+        stackNode();
+        stackNode(T data, stackNode<T> *next);
+        T getData();
+        stackNode<T> *getNext();
+        void setData(T data);
+        void setNext(stackNode<T> *next);
+        
+    private:
+        T data;
+        stackNode<T> *next;
+};
+//null constructor
+template <class T>
+stackNode<T>::stackNode(){
+    this->data = nullptr;
+    this->next = nullptr;
+}
+// Constructor
+template <class T>
+stackNode<T>::stackNode(T data, stackNode<T> *next){
+    this->data = data;
+    this->next = next;
+}
+
+//getters
+//get data
+template <class T>
+T stackNode<T>::getData(){
+    return this->data;
+}
+
+//get next 
+template <class T>
+stackNode<T> *stackNode<T>::getNext(){
+    return this->next;
+}
+
+//setters
+//set data
+template <class T>
+void stackNode<T>::setData(T data){
+    this->data = data;
+}
+
+//set next
+template <class T>
+void stackNode<T>::setNext(stackNode<T> *next){
+    this->next = next;
+}
+
+//line for readability
+//------------------------------------------------------
+
+//stack class
+template <class T>
+class stack{
+    public:
+        //stack utilities
+        //constructor
+        stack();
+        //destructor
+        ~stack();
+        //clear
+        void clear();
+        //------------------------------------------------------------------
+        //stack operations
+        //push
+        void push(T data);
+        //pop
+        T pop();
+        //peek
+        T peek();
+        //isEmpty
+        bool isEmpty();
+        //print stack
+        void print();
+        
+    private:
+        stackNode<T> *top;
+};
+
+//null constructor
+template <class T>
+stack<T>::stack(){
+    this->top = nullptr;
+}
+
+//destructor
+template <class T>
+stack<T>::~stack(){
+    stackNode<T> *temp;
+    while(this->top != nullptr){
+        temp = this->top;
+        this->top = this->top->getNext();
+        delete temp;
+    }
+}
+//clear
+template <class T>
+void stack<T>::clear(){
+    stackNode<T> *temp;
+    while(this->top != nullptr){
+        temp = this->top;
+        this->top = this->top->getNext();
+        delete temp;
+    }
+}
+//------------------------------------------------------------------
+//push
+template <class T>
+void stack<T>::push(T data){
+    stackNode<T> *temp = new stackNode<T>(data, this->top);
+    this->top = temp;
+}
+
+//pop
+template <class T>
+T stack<T>::pop(){
+    if(this->top == nullptr){
+        return -1;//####Correction, -1 = failed c function (i guess this works in windows. 
+    }
+    T data = this->top->getData();
+    stackNode<T> *temp = this->top;
+    this->top = this->top->getNext();
+    delete temp;
+    return data;
+}
+
+//peek
+template <class T>
+T stack<T>::peek(){
+    if(this->top == nullptr){
+        return -1;//####Correction, -1 = failed c function (i guess this works in windows. 
+    }
+    return this->top->getData();
+}
+
+
+//isEmpty
+template <class T>
+bool stack<T>::isEmpty(){
+    if(this->top == nullptr){
+        return true;
+    }
+    return false;
+}
+
+
+//print
+template <class T>
+void stack<T>::print(){
+    stackNode<T> *temp = this->top;
+    while(temp != nullptr){
+        cout << temp->getData() << endl;
+        temp = temp->getNext();
+    }
+}
+#endif
---
+++ Lab3_Corrections_V2_Nichols/searchSortAlgorithms.h
@@ -0,0 +1,254 @@
+/*
+'* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+'* Program name    : L3-3                                              * 
+'*                                                                     * 
+'* Written by      : Bailey Nichols                                    * 
+'*                                                                     * 
+'* Purpose         : To demonstrate diffrent sorting methods.          *
+'*---------------------------------------------------------------------* 
+'* Change Log:                                                         * 
+'*                         Revision                                    * 
+'*       Date    Changed  Rel Ver Mod Purpose                          * 
+'* 11/08/12      Bailey 000.000.000 Initial release of program       * 
+'*                                                                     * 
+'* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+*/
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures 
+//(MindTap Course List) (p. 1316). Cengage Learning Kindle Edition
+#ifndef SEARCH_SORT_ALGO_H
+#define SEARCH_SORT_ALGO_H
+
+
+template <class elmT>
+void swap(elmT list[], int first, int second) {
+    
+    elmT temp = list[first];
+    list[first] = list[second];
+    list[second] = temp;
+}//
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures 
+//(MindTap Course List) (p. 1281). Cengage Learning Kindle Edition
+template <class elmT>
+int seqSearch(const elmT list[], int size, const elmT &item)
+{
+    int loc;
+    bool found = false;
+    while (!found && loc < size)
+    {
+        if (list[loc] == item)
+            found = true;
+        else
+            loc++;
+    }
+    if (found)
+        return loc;
+    else
+        return -1;
+}
+
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures 
+//(MindTap Course List) (p. 1285). Cengage Learning Kindle Edition
+template <class elmT>
+int binarySearch(const elmT list[], int size, const elmT &item)
+{
+    int first = 0;
+    int last = size - 1;
+    int mid;
+    bool found = false;
+    while (!found && first <= last)
+    {
+        mid = (first + last) / 2;
+        if (list[mid] == item)
+            found = true;
+        else if (list[mid] > item)
+            last = mid - 1;
+        else
+            first = mid + 1;
+    }
+    if (found)
+        return mid;
+    else
+        return -1;
+}
+
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1300). Cengage Learning Kindle Edition
+template <class elmT>
+int bubbleSort(elmT list[], int size)
+{
+    for (int pass = 1; pass < size; pass++)
+    {
+        for (int loc = 0; loc < size - 1; loc++)
+        {
+            if (list[loc] > list[loc + 1])
+            {
+                elmT temp = list[loc];
+                list[loc] = list[loc + 1];
+                list[loc + 1] = temp;
+            }
+        }
+    }
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1303-1304). Cengage Learning Kindle Edition
+template <class elmT>
+int minLocation(elmT list[], int first, int last){
+    int minIndex = first;
+    for (int loc = first + 1; loc <= last; loc++)
+        if (list[loc] < list[minIndex])
+            minIndex = loc;
+    return minIndex;
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1304). Cengage Learning Kindle Edition
+template <class elmT>
+void selectionSort(elmT list[], int size)
+{
+    int minIndex;
+    for (int loc =0; loc < size - 1; loc++)
+    {
+        minIndex = minLocation(list, loc, size - 1);
+        swap(list, loc, minIndex);
+    }
+}
+
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1308). Cengage Learning Kindle Edition
+template <class elmT>
+void insertionSort(elmT list[], int size)
+{
+    for (int unsorted = 1; unsorted < size; unsorted++)
+    {
+        elmT nextItem = list[unsorted];
+        int loc = unsorted;
+        while (loc > 0 && list[loc - 1] > nextItem)
+        {
+            list[loc] = list[loc - 1];
+            loc--;
+        }
+        list[loc] = nextItem;
+    }
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1310). Cengage Learning Kindle Edition
+template <class elmT>
+int partition(elmT list[], int first, int last)
+{
+    elmT pivot;
+    int smallIndex;
+    swap(list, first, (first + last) / 2);
+    pivot = list[first];
+    smallIndex = first;
+    for (int loc = first + 1; loc <= last; loc++)
+        if (list[loc] < pivot)
+        {
+            smallIndex++;
+            swap(list, smallIndex, loc);
+        }
+    swap(list, first, smallIndex);
+
+    return smallIndex;
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1317). Cengage Learning Kindle Edition
+template <class elmT>
+void recQuickSort(elmT list[], int first, int last)
+{
+    if (first < last)
+    {
+        int pivotIndex = partition(list, first, last);
+        recQuickSort(list, first, pivotIndex - 1);
+        recQuickSort(list, pivotIndex + 1, last);
+    }
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1317). Cengage Learning Kindle Edition
+template <class elmT>
+void quickSort(elmT list[], int size)
+{
+    recQuickSort(list, 0, size - 1);
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1311-1316). Cengage Learning Kindle Edition
+template <class elmT>
+void quickSortMedianPivot(elmT list[], int size)
+{
+    int first = 0;
+    int last = size - 1;
+    int pivotIndex;
+    while (first < last)
+    {
+        pivotIndex = partition(list, first, last);
+        if (pivotIndex - first < last - pivotIndex)
+        {
+            recQuickSort(list, first, pivotIndex - 1);
+            first = pivotIndex + 1;
+        }
+        else
+        {
+            recQuickSort(list, pivotIndex + 1, last);
+            last = pivotIndex - 1;
+        }
+    }
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1311-1316). Cengage Learning Kindle Edition
+template <class elmT>
+void quickSortWithInsertionSort(elmT list[], int size)
+{
+    int first = 0;
+    int last = size - 1;
+    int pivotIndex;
+    while (first < last)
+    {
+        pivotIndex = partition(list, first, last);
+        if (pivotIndex - first < last - pivotIndex)
+        {
+            insertionSort(list, pivotIndex - first + 1);
+            first = pivotIndex + 1;
+        }
+        else
+        {
+            insertionSort(list, last - pivotIndex + 1);
+            last = pivotIndex - 1;
+        }
+    }
+}
+
+//Malik, D.S.. C++ Programming  Programming Design Including Data Structures
+//(MindTap Course List) (p. 1311-1316). Cengage Learning Kindle Edition
+template <class elmT>
+void quickSortMedianWithInsertionSort(elmT list[], int size)
+{
+    int first = 0;
+    int last = size - 1;
+    int pivotIndex;
+    while (first < last)
+    {
+        pivotIndex = partition(list, first, last);
+        if (pivotIndex - first < last - pivotIndex)
+        {
+            insertionSort(list, pivotIndex - first + 1);
+            first = pivotIndex + 1;
+        }
+        else
+        {
+            insertionSort(list, last - pivotIndex + 1);
+            last = pivotIndex - 1;
+        }
+    }
+}
+
+#endif
